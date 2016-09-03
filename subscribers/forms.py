89104d37fc34375ec75a12b1ac3a78bce30fb9bb
Britt Dawn <brittdawndev@gmail.com>
2016-09-03 13:54:49 -0700

    Improving Django admin and forms for subscribers app. Pair programmed with team.

---
+++ subscribers/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+
+from .models import SignUp
+
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = SignUp
+        fields = ['full_name', 'email']
--- subscribers/models.py
+++ subscribers/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Create your models here.
 class SignUp(models.Model):
     email = models.EmailField()
-    full_name = models.CharField(max_length=120, blank=True, null=True)
+    full_name = models.CharField(max_length=120, blank=False, null=True)
     timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True)
 
