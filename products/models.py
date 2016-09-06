ce42b175760220d31b6d0468c65daf3799a872fc
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-09-06 14:44:03 -0700

    added feature to expand product descriptions and product lists, added variation options, default options, and add to cart link

--- products/models.py
+++ products/models.py
@@ -1,14 +1,68 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+from django.db.models.signals import post_save
+
 
 # Creating our product models
+
+class ProductQuerySet(models.query.QuerySet):
+    def active(self):
+        return self.filter(active=True)
+
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return ProductQuerySet(self.model, using=self._db)
+
+    def all(self, *args, **kwargs):
+        return self.get_queryset().active()
+
+
 class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     active = models.BooleanField(default=True)
+    objects = ProductManager()
 
     def __unicode__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("product_detail", kwargs={"pk":self.pk})
+
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product)
+    title = models.CharField(max_length=120)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    sale_price = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+    active = models.BooleanField(default=True)
+    inventory = models.IntegerField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.title
+
+    def get_price(self):
+        if self.sale_price is not None:
+            return self.sale_price
+        else:
+            return self.price
+
+    def get_absolute_url(self):
+        return self.product.get_absolute_url()
+
+def product_post_saved_receiver(sender, instance, created, *args, **kwargs):
+    product = instance
+    variations = product.variation_set.all()
+    if variations.count() == 0:
+        new_var = Variation()
+        new_var.product = product
+        new_var.title = "Default"
+        new_var.price = product.price
+        new_var.save()
+
+post_save.connect(product_post_saved_receiver, sender=Product)
 
     #slug
     #inventory?
--- products/views.py
+++ products/views.py
@@ -11,17 +11,17 @@ from .models import Product
 
 class ProductListView(ListView):
     model = Product
+    queryset = Product.objects.all()
+
     def get_context_data(self, *args, **kwargs):
         context = super(ProductListView, self).get_context_data(*args, **kwargs)
         context["now"] = timezone.now()
         return context
 
+
 class ProductDetailView(DetailView):
     model = Product
     # template_name = "<appname>/<modelname>_detail.html"
-
-
-
 
 def product_detail_view_func(request, id):
     #product_instance = Product.objects.get(id=id)
