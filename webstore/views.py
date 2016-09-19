67d8949247f96bfb1c4c609d76fa6e9c013041cb
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-09-19 10:53:02 -0700

    fixing syntax for linter

--- webstore/views.py
+++ webstore/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 # =========================================================================
 # Renders about.html page
 # =========================================================================
+
 def about(request):
     return render(request, "about.html", {})
