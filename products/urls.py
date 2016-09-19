7573a69f5caf9cc1e6af329cdffadc54bd2cc14d
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-09-19 10:44:06 -0700

    comments for code in products folder

--- products/urls.py
+++ products/urls.py
@@ -4,6 +4,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from .views import ProductDetailView, ProductListView
 
+# =========================================================================
+# Enabling RESTful architecture with dynamic urls.
+# =========================================================================
+
 urlpatterns = [
     url(r'^(?P<pk>\d+)', ProductDetailView.as_view(), name='product_detail'),
     url(r'^$', ProductListView.as_view(), name='products'),
