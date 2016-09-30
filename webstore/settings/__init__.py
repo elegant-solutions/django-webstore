20a26d94d7aa7e932d236fb23301c756d75fc5e7
Rachael Wisecarver <rachael.wisecarver@gmail.com>
2016-09-30 12:27:16 -0700

    preparing for deployment

---
+++ webstore/settings/__init__.py
@@ -0,0 +1,21 @@
+from .base import *
+
+try:
+    from .local import *
+except:
+    pass
+
+try:
+    from .production import *
+except:
+    pass
+
+try:
+    from .imac import *
+except:
+    pass
+
+try:
+    from .macbookpro import *
+except:
+    pass
